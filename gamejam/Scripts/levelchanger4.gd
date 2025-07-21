3a349e5629b70ec71a5eb7c00caf32bfce6af3a0
GhOsT <137192416+GhOsTs369@users.noreply.github.com>
2025-07-21 14:00:10 +0530

    added last minute stuff

--- gamejam/Scripts/levelchanger4.gd
+++ gamejam/Scripts/levelchanger4.gd
@@ -1,5 +1,14 @@
 extends Area2D
 
+@onready var timer: Timer = $Timer
+@onready var panel: Panel = $"../CanvasLayer2/Panel"
+
+func _ready() -> void:
+	panel.visible = false
 
 func _on_body_entered(_body: CharacterBody2D) -> void:
-	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+	panel.visible = true
+	timer.start()
+
+func _on_timer_timeout() -> void:
+		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
